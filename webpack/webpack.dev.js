2596cc7323637ae8adcc7dc8554d36631b719ceb
Makesh <65646841+makesh-git@users.noreply.github.com>
2021-11-08 18:57:54 +0530

    added react-refresh to dev

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,12 +1,17 @@
 const webpack = require("webpack");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 module.exports = {
   mode: "development",
   devtool: "cheap-module-source-map",
+  devserver: {
+    hot: true
+  },
   plugins: [
     new webpack.DefinePlugin({
       "process.env.name": {
         NODE_ENV: JSON.stringify("makesh-development"),
       },
     }),
+    new ReactRefreshWebpackPlugin(),
   ],
 };
