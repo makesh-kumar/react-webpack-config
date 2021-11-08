552148d767388f61d27e9af350d4585de15f758e
Makesh <65646841+makesh-git@users.noreply.github.com>
2021-11-08 19:26:47 +0530

    added eslint and prettier plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import "./styles.css"
+import './styles.css'
 function App() {
-    return (
-        <div>
-            <h2>React Apps</h2>
-            <h3>{process.env.NODE_ENV}</h3>
-            <h3>{JSON.stringify(process.env.name)}</h3>
-        </div>
-    )
+  return (
+    <div>
+      <h2>React Apps</h2>
+      <h3>{process.env.NODE_ENV} </h3>
+      <h3>{JSON.stringify(process.env.name)}</h3>
+    </div>
+  )
 }
 
 export default App
